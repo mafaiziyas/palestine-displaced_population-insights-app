4f84af15f1b3715ac639c19a5479b315f0a88518
mafaiziyas <mohomedmafaiz@gmail.com>
2026-04-26 12:18:57 +0530

    data loading
    Added data loading and visualization libraries for the dashboard.

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Title of the app
+st.title("Palestine Displacement Data Dashboard (1976-2025)")
+
+# Loading csv data
+url = "https://raw.githubusercontent.com/mafaiziyas/mafaiziyas/palestine-displaced_population-insights-app/main/palestine_displacement_1976_2025.csv"
+df = pd.read_csv(url)
